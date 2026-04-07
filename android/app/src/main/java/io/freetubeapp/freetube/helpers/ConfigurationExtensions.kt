517f5d284e5b4777862f5ae287868d7e4c881d58
Emma <MarmadileManteater@proton.me>
2026-04-07 11:56:51 -0400

    Extract return from when

--- android/app/src/main/java/io/freetubeapp/freetube/helpers/ConfigurationExtensions.kt
+++ android/app/src/main/java/io/freetubeapp/freetube/helpers/ConfigurationExtensions.kt
@@ -3,15 +3,15 @@ package io.freetubeapp.freetube.helpers
 import android.content.res.Configuration
 
 fun Configuration.isDarkMode(): Boolean {
-  when (uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+  return when (uiMode and Configuration.UI_MODE_NIGHT_MASK) {
     Configuration.UI_MODE_NIGHT_NO -> {
-      return false
+      false
     }
     Configuration.UI_MODE_NIGHT_YES -> {
-      return true
+      true
     }
     else -> {
-      return false
+      false
     }
   }
 }
