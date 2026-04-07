a6c6484a66b1438803031a7ca080c36ef8953783
Emma <MarmadileManteater@proton.me>
2026-04-07 12:32:56 -0400

    Remove more warnings

--- android/app/src/main/java/io/freetubeapp/freetube/KeepAliveService.kt
+++ android/app/src/main/java/io/freetubeapp/freetube/KeepAliveService.kt
@@ -6,9 +6,7 @@ import android.app.NotificationManager
 import android.app.Service
 import android.content.ComponentName
 import android.content.Intent
-import android.os.Build
 import android.os.IBinder
-import androidx.core.app.NotificationChannelCompat
 import androidx.core.app.NotificationManagerCompat
 
 
